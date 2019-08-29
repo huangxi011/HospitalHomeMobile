0b5a9a1f4487ced22973326a0dbc36e7eba428b4
huangxi011 <huangxi_011@qq.com>
2019-08-29 19:26:08 +0800

    提交一个LOGO，改了一些文件使其编译通过

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ import App from './App'
 import cuCustom from './colorui/components/cu-custom.vue'
 import SB from './colorui/components/myStatusBar.vue'
 import bottomNavBar from './colorui/components/bottomNavBar.vue'
-Vue.component('cu-custom',cuCustom)
-Vue.component('statusBar',SB)
-Vue.component('bottom-navbar',bottomNavBar)
+Vue.component('cu-custom', cuCustom)
+Vue.component('statusBar', SB)
+Vue.component('bottom-navbar', bottomNavBar)
 
 Vue.config.productionTip = false
 
